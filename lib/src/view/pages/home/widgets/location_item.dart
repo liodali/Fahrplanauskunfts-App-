7aaf705be0bbd9f0372fc21d89059547a716fb1c
liodali <16631886+liodali@users.noreply.github.com>
2024-04-14 02:33:22 +0200

    improve location_item

--- lib/src/view/pages/home/widgets/location_item.dart
+++ lib/src/view/pages/home/widgets/location_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:search_repository/search_repository.dart';
+import 'package:timetabl_app/src/commons/commons.dart';
 import 'package:timetabl_app/src/view/pages/home/widgets/location_chips.dart';
 import 'package:timetabl_app/src/view/pages/home/widgets/location_is_best.dart';
 
@@ -13,24 +14,39 @@ class ListLocationItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Row(
-          children: [
-            Expanded(
-              child: Text(
-                location.name,
-                style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                      fontWeight: FontWeight.w700,
-                    ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  location.name,
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        fontWeight: FontWeight.w700,
+                      ),
+                ),
               ),
+              LocationIsBestWidget(
+                isBest: location.isBest,
+              ),
+            ],
+          ).padding(
+            const EdgeInsets.only(
+              top: 3,
+              bottom: 6,
             ),
-            LocationIsBestWidget(
-              isBest: location.isBest,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: LocationChips(
+              chipsLabel: location.buildContent(),
             ),
-          ],
-        ),
-        subtitle: LocationChips(
-          chipsLabel: location.buildContent(),
+          ),
+        ],
+      ).padding(
+        const EdgeInsets.symmetric(
+          vertical: 6,
+          horizontal: 12,
         ),
       ),
     );
